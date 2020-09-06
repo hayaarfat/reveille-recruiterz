af001d45334d0406a3802ffa31e44f2fc75b5caf
haya <70782256+hayaarfat@users.noreply.github.com>
2020-09-06 00:54:29 -0500

    Update Dashboard.vue

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="dashboard">
+    <section>
+      <div class="col1">
+        <div class="profile">
+          <h5>{{ userProfile.name }}</h5>
+          <p>{{ userProfile.title }}</p>
+          <div class="create-post">
+            <p>create a post</p>
+            <form @submit.prevent>
+              <textarea v-model.trim="post.content"></textarea>
+              <button @click="createPost()" :disabled="post.content === ''" class="button">post</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="col2">
+        <div>
+          <p class="no-results">There are currently no posts</p>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+
